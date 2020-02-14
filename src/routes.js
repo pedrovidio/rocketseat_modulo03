5e698c69ce26cd154b795b1bd34161a201ee4e37
Pedro Ovidio <pedrovidio@gmail.com>
2020-02-14 00:03:01 -0300

    aula listando agenda prestador

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import SchedulesController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -23,6 +24,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedules', SchedulesController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
